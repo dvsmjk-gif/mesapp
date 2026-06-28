3f1452f1d3b0c38f9a0b3eb47da8f122ada53599
dvsmjk-gif <dvsmjk@gmail.com>
2026-06-28 17:34:04 +0300

    fix client host for production

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,6 +2,7 @@ import { treaty } from '@elysia/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>
-("localhost:3000").api
+export const client = treaty<App>(
+  typeof window !== "undefined" ? window.location.host : "localhost:3000"
+).api
 
